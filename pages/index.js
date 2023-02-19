1fffdaa625616debec623a419528a4aed311cafd
hyeonuk-choo <hyeonuk97@naver.com>
2023-02-20 08:44:08 +0900

    test: Environment variables 설정 및 api통신 스터디

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,10 @@ export default function Home() {
       <ul>
         <li><a href="/sub">/pages/sub/index.js</a></li>
         <li><a href="/sub/about">/pages/sub/about.js</a></li>
-        <li><a href="/sub/1">/pages/sub/[first-any].js</a></li>
-        <li><a href="/sub/2">/pages/sub/[second-any].js</a></li>
+        <li><a href="/sub/1">/pages/sub/[any].js</a></li>
+        <li><a href="/sub/2">/pages/sub/[any].js</a></li>
+        <li><a href="/sub/문자, 공백특수문자~!?도 되나?">/pages/sub/[any].js</a></li>
+        <li><a href="/sub/fetch">/pages/sub/fetch.js</a></li>
       </ul>
     </>
   )
