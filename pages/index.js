aa5cee5eec43e3a63eb4e04327877cde252745c1
hyeonuk-choo <hyeonuk97@naver.com>
2023-02-22 18:21:01 +0900

    feat: Add getServerSideProps function and api

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,28 @@
-import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <>
-      <h1>/pages/index.js</h1>
+      <h1>here is My Webpage! Make yourself at home ^^</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_start=0&_end=10"
+  );
+  const posts = await res.json();
+
+  return { props: { posts } };
+};
